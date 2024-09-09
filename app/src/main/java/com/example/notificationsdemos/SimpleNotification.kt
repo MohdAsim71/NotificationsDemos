b4ca3a9d3f07ac4b6a6f8d1a70b5b91b1add298d
mohdaasim <mohdaasim71@gmail.com>
2024-09-09 12:26:03 +0530

    Initial commit

---
+++ app/src/main/java/com/example/notificationsdemos/SimpleNotification.kt
@@ -0,0 +1,32 @@
+package com.example.notificationsdemos
+
+import android.Manifest.permission.POST_NOTIFICATIONS
+import android.content.Context
+import android.content.pm.PackageManager
+import android.os.Build
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import androidx.core.content.ContextCompat
+import com.example.notificationsdemos.DEFAULT
+
+fun simpleNotification(context: Context) {
+
+    val notification = NotificationCompat.Builder(context, DEFAULT)
+        .setSmallIcon(R.drawable.ic_launcher_background)
+        .setContentTitle("title")
+        .setContentText("description")
+        .build()
+
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+        if (ContextCompat.checkSelfPermission(
+                context,
+                POST_NOTIFICATIONS
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            NotificationManagerCompat.from(context).notify(1,notification)
+        }
+    }else{
+        NotificationManagerCompat.from(context).notify(1,notification)
+    }
+
+}
